bf36221f22d1560d522375d3d0e394d768de8eaf
Mike Joseph <mike.joseph5@gmail.com>
2014-09-16 20:31:35 -0400

    fixed abstract error updated to most recent API path changes added debug mode to rest client

--- _add_ons/givingimpact/givingimpact-php/MODL/GivingImpact.php
+++ _add_ons/givingimpact/givingimpact-php/MODL/GivingImpact.php
@@ -4,27 +4,27 @@
  *
  * Simple, fluent interface to the Giving Impact API
  *
- * 	<pre>
- *  	$API = new \MODL\GivingImpact(
+ *  <pre>
+ *      $API = new \MODL\GivingImpact(
  *          'My-User-Agent', 'MY-GI-API-KEY'
  *      );
  *
  *      $campaign = $API
- *      	->campaign
- *      	->fetch('XXXXX');
+ *          ->campaign
+ *          ->fetch('XXXXX');
  *
  *      $stats = $campaign
- *      	->stats
- *      	->limit($max+1)
- *      	->offset($offset)
- *      	->fetch();
- * 	</pre>
+ *          ->stats
+ *          ->limit($max+1)
+ *          ->offset($offset)
+ *          ->fetch();
+ *  </pre>
  *
- * @package        	GivingImpact
- * @subpackage    	Libraries
- * @category    	Libraries
- * @author        	Minds on Design Lab, Inc.
- * @created			10/01/2012
+ * @package         GivingImpact
+ * @subpackage      Libraries
+ * @category        Libraries
+ * @author          Minds on Design Lab, Inc.
+ * @created         10/01/2012
  * @license         #
  */
 
@@ -53,86 +53,86 @@ require_once dirname(__FILE__).'/GivingImpact/Model/Stats.php';
  */
 class GivingImpact {
 
-	/**
-	 * Base components for dependency injector
-	 * @var array
-	 */
-	protected $components = array(
-		'end_point'		=> 'https://givingimpact.com/api',
-		'user_agent'	=> 'Test UA',
-		'api_key'		=> false,
-	);
+    /**
+     * Base components for dependency injector
+     * @var array
+     */
+    protected $components = array(
+        'end_point'     => 'https://app.givingimpact.com/api',
+        'user_agent'    => 'Test UA',
+        'api_key'       => false,
+    );
 
-	/**
-	 * Constructor
-	 * @param String $user_agent
-	 * @param String $api_key
-	 */
-	public function __construct($user_agent, $api_key) {
+    /**
+     * Constructor
+     * @param String $user_agent
+     * @param String $api_key
+     */
+    public function __construct($user_agent, $api_key) {
 
-		$this->user_agent = $user_agent;
-		$this->api_key = $api_key;
+        $this->user_agent = $user_agent;
+        $this->api_key = $api_key;
 
-		if( !function_exists('curl_init') ) {
-			throw new GIException('CURL extension is required');
-			return;
-		}
+        if( !function_exists('curl_init') ) {
+            throw new GIException('CURL extension is required');
+            return;
+        }
 
-		$v = curl_version();
-		if( !($v['features'] & CURL_VERSION_SSL) ) {
-			throw new GIException('SSL is required');
-			return;
-		}
+        $v = curl_version();
+        if( !($v['features'] & CURL_VERSION_SSL) ) {
+            throw new GIException('SSL is required');
+            return;
+        }
 
-		$this->initialize();
-	}
+        $this->initialize();
+    }
 
-	/**
-	 * Initialize the dependency injector. Loads the REST client
-	 * and models with base DI object.
-	 *
-	 */
-	public function initialize() {
+    /**
+     * Initialize the dependency injector. Loads the REST client
+     * and models with base DI object.
+     *
+     */
+    public function initialize() {
 
-		// build and return new rest client with proper settings
-		$this->restClient = function($_) {
-			$rc = new RestClient($_);
-			$rc->headers = array(
-				'X-GI-Authorization: '.$_->api_key,
-				'Content-Type: application/json'
-			);
+        // build and return new rest client with proper settings
+        $this->restClient = function($_) {
+            $rc = new RestClient($_);
+            $rc->headers = array(
+                'X-GI-Authorization: '.$_->api_key,
+                'Content-Type: application/json'
+            );
 
-			$rc->url = $_->end_point;
-			$rc->user_agent = $_->user_agent;
+            $rc->url = $_->end_point;
+            $rc->user_agent = $_->user_agent;
 
-			return $rc;
-		};
+            return $rc;
+        };
 
-		$this->campaign = function($_) {
-			return new Campaign($_);
-		};
-		$this->opportunity = function($_) {
-			return new Opportunity($_);
-		};
-		$this->donation = function($_) {
-			return new Donation($_);
-		};
-		$this->stats = function($_) {
-			return new Stats($_);
-		};
+        $this->campaign = function($_) {
+            return new Campaign($_);
+        };
+        $this->opportunity = function($_) {
+            return new Opportunity($_);
+        };
+        $this->donation = function($_) {
+            return new Donation($_);
+        };
+        $this->stats = function($_) {
+            return new Stats($_);
+        };
 
-	}
+    }
 
-	public function __get($k) {
-		if( is_callable($this->components[$k]) ) {
-			return $this->components[$k]($this);
-		}
+    public function __get($k) {
+        if( is_callable($this->components[$k]) ) {
+            return $this->components[$k]($this);
+        }
 
-		return $this->components[$k];
-	}
+        return $this->components[$k];
+    }
 
-	public function __set($k, $v) {
-		$this->components[$k] = $v;
-	}
+    public function __set($k, $v) {
+        $this->components[$k] = $v;
+    }
 
 }
